d5a99469aa2877d8da7b37b2a18ac9a346af6e4e
AshishRaj0602 <115613437+AshishRaj0602@users.noreply.github.com>
2023-03-25 09:23:48 +0530

    commit2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState ,useEffect, useMemo} from 'react';
+import { useState ,useEffect} from 'react';
 import Quiz from './component/quizbody/Quiz';
 import { Data } from './Data';
 import Timer from './component/timer/Timer';
@@ -7,10 +7,10 @@ function App() {
   const [queNum, setqueNum] = useState(1);
   const [stop, setStop] = useState(false);
   const [earned,setEarned] =useState(0);
-  const priceArr=[{id:1,price:100},{id:2,price:200},{id:3,price:300},{id:4,price:500},{id:5,price:1000},{id:6,price:2000},{id:7,price:4000},{id:8,price:8000},{id:9,price:16000},{id:10,price:32000},{id:11,price:64000},{id:12,price:120000},{id:13,price:250000},{id:14,price:1000000}].reverse();
-  const priceObj=useMemo(()=>
-    priceArr
-  ,[])
+  const priceObj=[{id:1,price:100},{id:2,price:200},{id:3,price:300},{id:4,price:500},{id:5,price:1000},{id:6,price:2000},{id:7,price:4000},{id:8,price:8000},{id:9,price:16000},{id:10,price:32000},{id:11,price:64000},{id:12,price:120000},{id:13,price:250000},{id:14,price:1000000}].reverse();
+  // const priceObj=useMemo(()=>
+  //   priceArr
+  // ,)
   useEffect(() => {
 
     queNum > 1 && setEarned(priceObj.find((x)=> x.id ===queNum-1)?.price);
